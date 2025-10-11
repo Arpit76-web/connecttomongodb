9087b2e52f47da795b519a4df7a66f2bb8d840f2
Arpit76-web <singh7898arpit@gmail.com>
2025-10-12 00:40:03 +0530

    make new folder and file

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const mongoose = require('mongoose');
+const connectDB = require('./db');
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect('mongodb://localhost:27017/', {
-            useNewUrlParser: true,
-        });
-        console.log(`MongoDB Connected`);
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-    }   
-};    
+// Connect to the database
+connectDB();
+
+//body parser middleware
+app.use(express.json());
+  
 
 app.get('/', (req, res) => {
   res.send('Hello World!, this is my first Express app.');
@@ -22,3 +17,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 }); 
+
---
+++ routes/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../model/userModel");
+
+//routes
+
+//CRUD Operations
+
+
+
+
+
+
+module.exports = router;
