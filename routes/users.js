6859fbff51cd229d08a2268c5018166f0cb8580f
Arpit76-web <singh7898arpit@gmail.com>
2025-10-19 23:44:06 +0530

    new commit

--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,12 @@ const User = require("../model/userModel");
 //view/read
 router.get("/users", async(req,res)=>{
     try{
-        const users = await User.find();
-        res.status(200).json(users);
+      const users = await user.find();
+        res.status(200).json({users})
     }
     catch(err){
         res.status(500).json({
-            success: false ,
+            success: false,
             message: err.message
         })
     }
